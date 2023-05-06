ed725179b55cf64a6eea0fbf8ff87c49948cd892
Western8 <westernchrome@gmail.com>
2023-05-06 12:16:00 +0300

    feat: add func 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const dirSrc = path.join(__dirname, 'files');
+const dirDest = path.join(__dirname, 'files-copy');
 
+function showError(err) {
+  if (err) {
+    throw err;
+  }  
+}
+
+function copyFiles() {
+  fs.readdir(dirSrc, (err, files) => {
+    files.forEach(file => {
+      fs.copyFile(path.join(dirSrc, file), path.join(dirDest, file), () => {});
+    })
+  });
+}
+
+fs.access(dirDest, (err) => {
+  if (err) {
+    fs.mkdir(dirDest, {recursive: true}, showError);
+    copyFiles();
+  } else {
+    fs.readdir(dirDest, (err, files) => {
+      files.forEach(file => {fs.unlink(path.join(dirDest, file), showError)})
+      copyFiles();
+      });
+  };
+})
